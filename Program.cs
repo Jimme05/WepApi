57595589d1b892403b88fb5d2dbc181afd0bccf7
Jimme05 <tiwtiwggtv@gmail.com>
2025-10-05 21:06:11 +0700

    ฟก

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using SimpleAuthBasicApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
     var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
@@ -14,6 +15,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
     )
 );
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") // 👈 Angular dev server
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials(); // ถ้ามี cookie/session
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -32,6 +45,7 @@ app.MapGet("/db-ping", async (AppDbContext db) =>
         return Results.Problem(ex.Message);
     }
 });
+app.UseCors("AllowFrontend");
 app.UseSwagger();
 app.UseSwaggerUI();
 
